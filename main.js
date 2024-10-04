91d792a1d0246b09ea35f761ed7c89c4e3b580ee
Duda Chambó <160850761+dgcoxinhas@users.noreply.github.com>
2024-10-04 12:05:07 -0300

    z

--- main.js
+++ main.js
@@ -26,9 +26,14 @@
 
 let estados= ['Acre', 'Alagoas', 'Amapa', 'Amazonas', 'Bahia', 'Ceará', 'Espírito Santo', 'Goiás', 'Maranhão', 'Mato Grosso', 'Mato Grosso do Sul', 'Minas Gerais', 'Pará', 'Paraíba', 'Paraná', 'Pernambuco', 'Piauí', 'Rio de Janeiro', 'Rio Grande do Norte', 'Rio Grande do Sul', 'Rondônia', 'Roraima', 'Santa Catarina', 'São Paulo', 'Sergipe', 'Tocantins', 'Distrito Federal'];
 
-let i=0;
-while(i<estados.length){
-console.log(i);
-console.log(estados [i])
-i= i+1;
+// let i=0;
+// while(i<estados.length){
+// console.log(i);
+// console.log(estados [i])
+// i= i+1;
+// }
+
+for (let j = 0; j < estados.length; j++) {
+   console.log(estados[j])
+    
 }
