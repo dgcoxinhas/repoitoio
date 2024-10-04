049321c5ef2fc84d9f53db3fb7c43265d9fb229f
Duda Chambó <160850761+dgcoxinhas@users.noreply.github.com>
2024-10-04 11:44:28 -0300

    j

--- main.js
+++ main.js
@@ -25,3 +25,9 @@
 //     document.write(media(notas1, nota2, notas3));
 
 let estados= ['Acre', 'Alagoas', 'Amapa', 'Amazonas', 'Bahia', 'Ceará', 'Espírito Santo', 'Goiás', 'Maranhão', 'Mato Grosso', 'Mato Grosso do Sul', 'Minas Gerais', 'Pará', 'Paraíba', 'Paraná', 'Pernambuco', 'Piauí', 'Rio de Janeiro', 'Rio Grande do Norte', 'Rio Grande do Sul', 'Rondônia', 'Roraima', 'Santa Catarina', 'São Paulo', 'Sergipe', 'Tocantins', 'Distrito Federal'];
+
+let i=0;
+while(i<10){
+console.log(i);
+i= i+1;
+}
