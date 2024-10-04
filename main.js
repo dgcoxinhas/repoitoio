fc48adf8e6367d330810fd7d3c8b89403b8a71f1
Duda Chambó <160850761+dgcoxinhas@users.noreply.github.com>
2024-10-04 11:24:07 -0300

    c

--- main.js
+++ main.js
@@ -1,24 +1,27 @@
-function soma(num1, num2){
-    return num1+num2;
-}
+// function soma(num1, num2){
+//     return num1+num2;
+// }
 
-console .log(soma(5,9));
+// console .log(soma(5,9));
 
-function areaQuadrado(ladoBase){
-    return ladoBase**2;
-}
+// function areaQuadrado(ladoBase){
+//     return ladoBase**2;
+// }
 
-console .log(areaQuadrado(8));
+// console .log(areaQuadrado(8));
 
-let media = function(valor1, valor2, valor3){
-    return (valor1+valor2+valor3)/3;
-}
+// let media = function(valor1, valor2, valor3){
+//     return (valor1+valor2+valor3)/3;
+// }
 
-console .log(media(1,2,3));
+// console .log(media(1,2,3));
 
-let nota1=parseInt(prompt('qual e a sua nota?'))
-let nota2=parseInt(prompt('qual e a sua nota?'))
-let nota3=parseInt(prompt('qual e a sua nota?'))
-let media= function(valor1, valor2, valor3){
-    return (valor1+valor2+valor3)/3;}
-    document.write(media(notas1, nota2, notas3))
+// let nota1=parseInt(prompt('qual e a sua nota?'))
+// let nota2=parseInt(prompt('qual e a sua nota?'))
+// let nota3=parseInt(prompt('qual e a sua nota?'))
+// let media= function(valor1, valor2, valor3){
+//     return (valor1+valor2+valor3)/3;
+// }
+//     document.write(media(notas1, nota2, notas3));
+
+let estados= ['Acre', 'Alagoas', 'Amazonas', 'Bahia', 'Ceará', 'Espírito Santo', 'Goiás', 'Maranhão', 'Mato Grosso', 'Mato Grosso do Sul', 'Minas Gerais', 'Pará', 'Paraíba', 'Paraná', 'Pernambuco', 'Piauí', 'Rio de Janeiro', 'Rio Grande do Norte', 'Rio Grande do Sul', 'Rondônia', 'Roraima', 'Santa Catarina', 'São Paulo', 'Sergipe', 'Tocantins', 'Distrito Federal'];
